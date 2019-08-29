038e8621563b2c692da68190937ed71f8cc76c3e
Anastassia Sytnik <anastassia_sytnik@yahoo.com>
2019-08-29 14:28:04 -0700

    Initial Maven project check-in

---
+++ binary-logger-SM/src/main/java/homework/binary_logger_SM/App.java
@@ -0,0 +1,13 @@
+package homework.binary_logger_SM;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

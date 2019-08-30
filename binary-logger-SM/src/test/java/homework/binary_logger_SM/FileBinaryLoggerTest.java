f9c9bb6e9b6161e21da5edaf1657c1501098560f
Anastassia Sytnik <anastassia_sytnik@yahoo.com>
2019-08-30 10:58:08 -0700

    Test stubs.

---
+++ binary-logger-SM/src/test/java/homework/binary_logger_SM/FileBinaryLoggerTest.java
@@ -0,0 +1,28 @@
+package homework.binary_logger_SM;
+
+import junit.framework.TestCase;
+
+
+public class FileBinaryLoggerTest extends TestCase {
+
+  public void testWrite() {
+    fail("Not yet implemented");
+  }
+
+  public void testRead() {
+    fail("Not yet implemented");
+  }
+
+  public void testFileBinaryLogger() {
+    fail("Not yet implemented");
+  }
+
+  public void testClose() {
+    fail("Not yet implemented");
+  }
+
+  public void testIntToBytes() {
+    fail("Not yet implemented");
+  }
+
+}
---
+++ binary-logger-SM/src/test/java/homework/binary_logger_SM/FileBinaryReaderTest.java
@@ -0,0 +1,24 @@
+package homework.binary_logger_SM;
+
+import junit.framework.TestCase;
+
+
+public class FileBinaryReaderTest extends TestCase {
+
+  public void testFileBinaryReader() {
+    fail("Not yet implemented");
+  }
+
+  public void testHasNext() {
+    fail("Not yet implemented");
+  }
+
+  public void testNext() {
+    fail("Not yet implemented");
+  }
+
+  public void testClose() {
+    fail("Not yet implemented");
+  }
+
+}

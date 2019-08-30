a96559541c9c1eb35ca6879f530895a23f5c3305
Anastassia Sytnik <anastassia_sytnik@yahoo.com>
2019-08-30 00:14:12 -0700

    Implement read/write and close.

--- binary-logger-SM/src/main/java/homework/binary_logger_SM/FileBinaryLogger.java
+++ binary-logger-SM/src/main/java/homework/binary_logger_SM/FileBinaryLogger.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
@@ -29,11 +31,16 @@ import java.util.List;
  */
 public class FileBinaryLogger<T extends BinaryLoggable> extends BinaryLogger<T> {
   
-  /** to write to the file */
-  private FileOutputStream output = null;
+  protected final static int TWO_INTEGERS = Integer.BYTES * 2;
   
+  /** to write to the file */
+  protected FileOutputStream output = null;
+
   /** to be able to read (FAST) only the certain sub-type. */
-  private HashMap<String, List<Integer>> typeMap = new HashMap<String, List<Integer>>();
+  protected HashMap<String, List<Long>> typeMap = new HashMap<String, List<Long>>();
+
+  /** amount of bytes already written */
+  protected long position = 0L;
 
   /****************************************************************************
    * Checks whether the provided file exists, readable and writable.
@@ -82,27 +89,105 @@ public class FileBinaryLogger<T extends BinaryLoggable> extends BinaryLogger<T> 
       }
     }
     
-    // This is a question whether we should open the file for writing right away.
-    // This would depend on frequency of writes. If it is frequent - we better keep it open.
-    // Otherwise we could check for null and open on first write.
-    this.output = new FileOutputStream(file, true);
   }
 
-  public void close() throws Exception {
-    // TODO Auto-generated method stub
-    
+  /**
+   * {@inheritDoc}
+   */
+  public void close() throws IOException {
+    // QUESTION: Should we also close the issued Iterators??
+    // For now we leave to to caller of the "read" method.
+    try {
+      if (null != this.output) {
+        this.output.flush();
+        this.output.close();
+        this.output = null;
+      }
+    } catch (IOException e) {
+      throw new IOException("Couldn't close output stream.", e);
+    }
   }
 
   @Override
   void write(T loggable) throws IOException {
-    // TODO Auto-generated method stub
-    
+    if (null == loggable) {
+      return;
+    }
+    // Re-open stream if it was closed earlier.
+    if (null == this.output) {
+      this.output = new FileOutputStream(this.outputFile, true);
+    }
+    String className = loggable.getClass().getCanonicalName();
+    byte[] nameBytes = className.getBytes(StandardCharsets.UTF_8);
+    int nameLen = nameBytes.length - 2;
+    byte[] data = loggable.toBytes();
+    byte[] numbers = new byte[TWO_INTEGERS];
+    intToBytes(nameLen, numbers, 0);
+    intToBytes(data.length, numbers, Integer.BYTES);
+    this.output.write(numbers);
+    this.output.write(nameBytes, 2, nameLen);
+    this.output.write(data);
+    this.output.flush();
+    this.position += TWO_INTEGERS + nameLen + data.length;
+    List<Long> crumbs = this.typeMap.get(className);
+    if (null == crumbs) {
+      crumbs = new ArrayList<Long>();
+      this.typeMap.put(className, crumbs);
+    }
+    crumbs.add(position);
   }
 
   @Override
   Iterator<T> read(Class<T> clazz) throws IOException {
     // TODO Auto-generated method stub
     return null;
+  }
+
+  /****************************************************************************
+   * Converts an integer to a byte array.
+   *
+   * This method is stolen from java.io.Bits.putInt().
+   * Unfortunately both the method and the class are not visible or could call.
+   *
+   * WARNING: this method is not fool-proof for speed. Please use with care:
+   * make sure the buffer length is sufficient and the offset is at least
+   * 4 positions from the end of the buffer.
+   *
+   * @param value - the integer to convert to bytes
+   * @param buffer - the byte array to put the converted integer to.
+   *     Make sure it's not null and at least Integer.BYTES long.
+   * @param offset - the offset from which the converted integer should start.
+   *       Make sure the offset is at least buffer.length - Integer.BYTES
+   */
+  public static void intToBytes(int value, byte[] buffer, int offset) {
+    buffer[offset + 3] = (byte) (value       );
+    buffer[offset + 2] = (byte) (value >>>  8);
+    buffer[offset + 1] = (byte) (value >>> 16);
+    buffer[offset    ] = (byte) (value >>> 24);
+  }
+
+  /****************************************************************************
+   * Reads the integer from byte array buffer.
+   *
+   * This method is stolen from java.io.Bits.getInt().
+   * Unfortunately both the method and the class are not visible or could call.
+   *
+   * WARNING: this method is not fool-proof for speed. Please use with care:
+   * make sure the offset is at least 4 positions from the end of the buffer.
+   * (4 == Integer.BYTES)
+   *
+   * @param buffer - the byte array containing integer in question.
+   *       Make sure it's not null and at least Integer.BYTES long.
+   * @param offset - the position in the buffer where the integer starts.
+   *       Make sure the offset is at least buffer.length - Integer.BYTES
+   *
+   * @return the value of the integer.
+   */
+  public static int bytesToInt(byte[] buffer, int offset) {
+    return ((buffer[offset + 3] & 0xFF)      ) +
+           ((buffer[offset + 2] & 0xFF) <<  8) +
+           ((buffer[offset + 1] & 0xFF) << 16) +
+           ((buffer[offset    ]       ) << 24);
   }
 
 }
